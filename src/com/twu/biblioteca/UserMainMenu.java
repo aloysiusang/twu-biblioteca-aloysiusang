ee2955a5a16ab4d0a34aaa28c316e9c931832968
Aloysius Ang <aloysiusang1990@gmail.com>
2015-06-17 19:42:15 +0800

    Implemeted User Main Menu

---
+++ src/com/twu/biblioteca/UserMainMenu.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aloysiusang on 17/6/15.
+ */
+public class UserMainMenu extends MainMenu {
+
+    public UserMainMenu() {
+        super(new ArrayList<MainMenuOption>() {{
+            add(new ListBooksOption());
+            add(new CheckOutBookOption());
+            add(new ReturnBookOption());
+            add(new ListMoviesOption());
+            add(new CheckOutMovieOption());
+            add(new ReturnMovieOption());
+            add(new UserInformationOption());
+            add(new QuitOption());
+        }});
+    }
+}
---
+++ test/com/twu/biblioteca/UserMainMenuTest.java
@@ -0,0 +1,46 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aloysiusang on 17/6/15.
+ */
+public class UserMainMenuTest {
+    private Set<Class> menuClasses;
+
+    @Before
+    public void setUp() throws Exception {
+        menuClasses = new HashSet<Class>() {{
+            add(CheckOutMovieOption.class);
+            add(QuitOption.class);
+            add(ListBooksOption.class);
+            add(UserInformationOption.class);
+            add(ReturnBookOption.class);
+            add(CheckOutBookOption.class);
+            add(ListMoviesOption.class);
+            add(ReturnMovieOption.class);
+        }};
+    }
+
+    @Test
+    public void testGetOptions() throws Exception {
+        UserMainMenu userMainMenu = new UserMainMenu();
+        ArrayList<MainMenuOption> options = userMainMenu.getOptions();
+        for(MainMenuOption option : options) {
+            if(menuClasses.contains(option.getClass())) {
+                menuClasses.remove(option.getClass());
+            }
+            else {
+                assertTrue("User Main Menu does not contain expected classes", false);
+            }
+        }
+        assertTrue("User Main Menu does not contain expected classes", menuClasses.size() == 0);
+    }
+}
